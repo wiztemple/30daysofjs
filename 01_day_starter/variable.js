6c3f0625ba64ebb2fc18591038a941e9fc95dc6d
wiztemple <wiztemple7@gmail.com>
2021-01-07 12:50:12 +0100

    Day 1: Introduction

---
+++ 01_day_starter/variable.js
@@ -0,0 +1,13 @@
+// Declaring different variables of different data types
+let firstName = 'Sullivan'
+let lastName = 'Wisdom'
+let country = 'Nigeria'
+let city = 'Aboh Mbaise'
+let age = 26
+let isMarried = true
+
+const gravity = 9.81
+let boilingPoint = 100
+const PI = 3.14
+
+let username = 'wiztemple', job = 'Frontend Engineer', live = 'San Francisco'
